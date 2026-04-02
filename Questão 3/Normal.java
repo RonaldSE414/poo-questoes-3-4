95d76e6926639ba7963a1fbfb108f531a2dd7b0b
RonaldSE414 <ronaldmachado592@gmail.com>
2026-04-02 02:14:34 -0300

    Criar Normal.java

---
+++ Questão 3/Normal.java
@@ -0,0 +1,10 @@
+public class Normal extends Ingresso {
+
+    public Normal(double valor) {
+        super(valor);
+    }
+
+    public void tipoIngresso() {
+        System.out.println("Ingresso Normal");
+    }
+}
